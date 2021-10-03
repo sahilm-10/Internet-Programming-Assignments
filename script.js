4f46f4bf1f3d63dcead5310aad493635eb89ee5f
sahilm-10 <82040432+sahilm-10@users.noreply.github.com>
2021-10-03 21:18:24 +0530

    Add files via upload
    Added and Modified Changes.

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-// Creating a Student Object
-const student1 = {
-    name: "Joe",
-    class_name: "D15A",
-    roll_no: 12,
-};
-console.log("Student 1: ", student1);
-class Student {
-    constructor(name, class_name, roll_no) {
-        this.name = name;
-        this.class_name = class_name;
-        this.roll_no = roll_no;
-} }
-const student2 = new Student("Jane", "D15A", 11);
-console.log("Student 2: ", student2);
-// Use of Prototype
-Student.prototype.nationality = "English";
-console.log("Student 2: ", student2);
-// Generating an Exception
-function checkFunction() {
-    const value = document.getElementById("error_checking").value;
+
+   class Student {
+    constructor(firstname, lastname, roll_no) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.roll_no = roll_no;
+   } 
+}
+
+const student = new Student("Sahil","Malgundkar",19);
+console.log(student);
+// document.write(student);
+
+const object2 = Object.getPrototypeOf(student)
+console.log(object2);
+
+console.log(student.firstname);
+console.log(student.lastname);
+console.log(student.roll_no);
+
+
+   function checkFunction() {
+    const value = document.getElementById("text-input").value;
     console.log(value);
     if (isNaN(value) || value == 0) {
-        throw new Error("Invalid Input ");
+    throw new Error("Invalid Input ");
     } else {
-        console.log("Valid");
+    console.log("Valid");
     }
-} 
+   }
